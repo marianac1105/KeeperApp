42988a4aed5dbd9f8f684fcc5ec9d15f22844b80
Mariana <marianac1105@gmail.com>
2020-10-30 13:49:00 +1100

    styled

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import Zoom from '@material-ui/core/Zoom';
 
 
 function CreateArea(props) {
+const [isClicked, setClick] = useState(false)
+function clickHandler () {
+  setClick(true)
+}
+
   return (
     <div className="form">
       
-        <input onChange = {props.titleInputHandler}  placeholder="Title" value = {props.titleValue} />
-        <textarea onChange = {props.textInputHandler}  placeholder="Take a note..." rows="3" value = {props.textValue}/>
+        <input onChange = {props.titleInputHandler}  placeholder="Title" value = {props.titleValue} style={isClicked? null: {display:"none"}}/>
+        <textarea onClick ={clickHandler} onChange = {props.textInputHandler}  placeholder="Take a note..." rows={isClicked?"3" : "1"} value = {props.textValue}/>
+        <Zoom in={isClicked}>
         <Fab className="addbutton" onClick = {() => {
         props.buttonHandler(props.noteItem)}}>
       <AddIcon/>
     </Fab>
-      
+    </Zoom>
     </div>
   );
 }
